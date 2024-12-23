57ceb6602363d1b484efba8ce49c1e8667808f43
Victor Okoro <okorovictor185@gmail.com>
2024-12-23 10:27:42 -0800

    update

---
+++ app/(store)/search/page.tsx
@@ -0,0 +1,12 @@
+async function SearchPage({
+    searchParams,
+}: {
+    searchParams: {
+        query: string;
+    };
+}) {
+  const { query } = await searchParams;
+  return <div>SearchPage for { query }</div>
+}
+
+export default SearchPage
